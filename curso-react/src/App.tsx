39e0a96c3fc9b0ca4c36eac94482cf0bcc49f167
LucasSalees <saleslucaas01@gmail.com>
2026-01-19 11:43:12 -0300

    FEAT: entendendo a estrutura básica do react, como funcionam os compnentes

--- curso-react/src/App.tsx
+++ curso-react/src/App.tsx
@@ -1,9 +1,37 @@
+interface IcardProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const Card = (props: IcardProps) => {
+  return (
+    <div style={{ border: '1px solid black', padding: '10px' }}>
+      <span>
+        title: {props.title}
+      </span>
+
+      <div>
+        {props.children}
+      </div>
+
+      <div>
+        footer
+      </div>
+    </div>
+  )
+}
 
 export function App() {
   
   return (
-    <>
-      <p> Olá, Mundo! </p>
-    </>
+    <div>
+
+      <p>Card: </p>
+
+      <Card title='teste'>
+        Abacate
+      </Card>
+
+    </div>
   )
 }
